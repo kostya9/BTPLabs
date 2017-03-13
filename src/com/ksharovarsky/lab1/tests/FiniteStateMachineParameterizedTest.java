9dd162a2884e408085cfc18b85f26fadcf37cab3
kostya9 <kostya9@gmail.com>
2017-03-14 01:43:38 +0200

    Recovered removed files while changing git naming case

---
+++ src/com/ksharovarsky/lab1/tests/FiniteStateMachineParameterizedTest.java
@@ -0,0 +1,21 @@
+package com.ksharovarsky.lab1.tests;
+
+import org.junit.Test;
+import org.junit.experimental.theories.Theories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kostya on 3/14/2017.
+ */
+@RunWith(Parameterized.class)
+public class FiniteStateMachineParameterizedTest {
+
+    @Test
+    public void scanString() throws Exception {
+
+    }
+
+}
